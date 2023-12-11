a200f4cca9bd914aca7725a78559e73830c0eb42
Thomas4 <tom.birchler@hotmail.com>
2023-12-11 09:22:45 +0000

    socket connection works

--- finetune/adapter.py
+++ finetune/adapter.py
@@ -67,8 +67,9 @@ def main(
 ):
 
     fabric = L.Fabric(
-        accelerator="cuda", 
-        devices=devices, 
+        # accelerator="cuda",
+        accelerator="cpu",
+        devices=devices,
         strategy=(DeepSpeedStrategy(config=ds_config) if devices > 1 else "auto"), 
         precision="bf16-true",
     )
